302b26e114c39c9b0510abd0f500ba95b0a30aa0
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-07-12 22:58:39 +0700

    Set up prisma

---
+++ prisma/migrations/20240712155753_user_schema/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `no_hp` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `photo_url` VARCHAR(191) NULL,
+    `role` ENUM('driver', 'user') NOT NULL DEFAULT 'user',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
