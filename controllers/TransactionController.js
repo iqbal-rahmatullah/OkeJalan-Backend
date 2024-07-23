529dd1a0c1604459a15b0a64cf126ee2fdce552d
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-07-23 23:50:47 +0700

    feat: add calculate price

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,19 +1,28 @@
+import axios from "axios"
+
 class TransactionController {
   static async getPrice(req, res) {
     try {
       const { originLat, originLng, destinationLng, destinationLat } = req.body
 
-      const distance = calcCrow(
-        originLat,
-        originLng,
-        destinationLat,
-        destinationLng
+      const calculate = await axios.get(
+        `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${originLat},${originLng}&destinations=${destinationLat},${destinationLng}&key=${process.env.DISTANCE_MATRIX_API_KEY}`
       )
+      const distance = calculate.data.rows[0].elements[0].distance.value
+      const distanceAsString = calculate.data.rows[0].elements[0].distance.text
+      const durationAsSecond = calculate.data.rows[0].elements[0].duration.value
+      const durationAsMinute = calculate.data.rows[0].elements[0].duration.text
+
+      const price = 4000 * (distance / 1000)
 
       return res.status(200).json({
         message: "Successfully get price",
         data: {
           distance,
+          distanceAsString,
+          durationAsSecond,
+          durationAsMinute,
+          price,
         },
       })
     } catch (error) {
